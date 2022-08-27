ad5f3127b0cf5cde478f038efbe2708a4a4ce97b
Tonny Blair Okumu <hellotony2034@gmail.com>
2022-08-27 10:25:17 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -11,6 +11,19 @@ const tutorials = [
   'what is JSONP?'
 ];
 
-const titleCased = () => {
-  return tutorials
+//const titleCased = () => {
+ // return tutorials
+//}
+function titleCased() {
+  const cased = tutorials.map((part) => {
+    const newCase = part
+    .split(' ')
+    .map((word) => {
+    return word.charAt(0).toUpperCase() + word.slice(1)
+    })
+    .join(' ')
+
+      return newCase
+  })
+  return cased
 }
